46b374d67366bfb17994b3de0329d737bbe65a2b
Dave <114460142+xtal10@users.noreply.github.com>
2026-01-15 09:50:03 +0800

    Delete M4_Activities/Seatwork6 directory

--- M4_Activities/Seatwork6/BankTestOperation.java
+++
@@ -1,8 +0,0 @@
-package Activity.M4_Activities.Seatwork6;
-
-
-@FunctionalInterface
-public interface BankTestOperation  {
-	
-	void execute() throws InvalidAmountException, InsufficientFundsException;
-}
