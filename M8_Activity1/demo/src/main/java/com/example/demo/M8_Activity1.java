bd6f78e6f136f942f194b04b899b93f8cf9d431e
Christian Dave S. Cania <gstcscania@robinsonsbank.com.ph>
2026-02-24 14:08:02 +0800

    M8_Activity1

---
+++ M8_Activity1/demo/src/main/java/com/example/demo/M8_Activity1.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M8_Activity1 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M8_Activity1.class, args);
+		System.out.print("Sample Springboot Program.");
+	}
+
+}
