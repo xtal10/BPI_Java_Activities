b7964bd19e94e5ebec37472f17a8e6cacc5a9600
Christian Dave S. Cania <gstcscania@robinsonsbank.com.ph>
2026-03-04 09:53:52 +0800

    M8_Activity6

---
+++ Exercise6/SingletonService.java
@@ -0,0 +1,13 @@
+package com.example.Exercise6;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SingletonService {
+
+	public void print() {
+		
+	
+		System.out.println("SingletonService Created");
+		
+	}
+}
